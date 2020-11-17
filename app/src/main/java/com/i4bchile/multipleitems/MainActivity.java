cd50a2032a31aa47d70e7c2069a7c57aa1f207ff
Jose Luis Ojeda <joseluisojedafdez@gmail.com>
2020-11-17 19:46:34 -0300

    Funciona correctamente. Toma la foto y la almacena en el image view, comprueba que existe la foto antes de pasar a la segunda actividad y toma los datos de la primera actividad para los intents de la segunda.

--- app/src/main/java/com/i4bchile/multipleitems/MainActivity.java
+++ app/src/main/java/com/i4bchile/multipleitems/MainActivity.java
@@ -1,12 +1,15 @@
 package com.i4bchile.multipleitems;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
@@ -19,65 +22,62 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final int REQUEST_TAKE_PHOTO = 1;
+    boolean cargaImagen=false;
     Button sacarFoto;
     Button avanzar;
-    String currentPhotoPath;
+    ImageView imagenACargar;
+    Intent takePictureIntent;
 
     private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
+        takePictureIntent = new
+                Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
 
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
+        }
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent
+            data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imagenACargar.setImageBitmap(imageBitmap);
         }
     }
 
-
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-
-
     private void iniciarBotones() {
-        sacarFoto = findViewById(R.id.bt_CargarImagen);
-        avanzar=findViewById(R.id.bt_next);
+
         sacarFoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dispatchTakePictureIntent();
+                cargaImagen=true;
+
+
+
             }
         });
         avanzar.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            if (cargaImagen==true) {
+                String website="https://desafiolatam.com";
+                Intent resultadoActividad= new Intent(getApplication().
+                        getBaseContext().
+                        getApplicationContext(),
+                        ResultActivity.class);
+                resultadoActividad.putExtra( "WEBSITE" ,website);
+                resultadoActividad.putExtra("IMAGEN CARGADA",cargaImagen);
+                startActivity(resultadoActividad);
 
+            }
+            else {
+                Toast.makeText(getApplicationContext(),"You should upload a picture before continuing", Toast.LENGTH_SHORT).show();
+
+            }
         }
         });
     }
@@ -87,7 +87,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imagenACargar=findViewById(R.id.img_ImagenCargada);
+        sacarFoto = findViewById(R.id.bt_CargarImagen);
+        avanzar=findViewById(R.id.bt_next);
         iniciarBotones();
+
 
 
     }
--- app/src/main/java/com/i4bchile/multipleitems/ResultActivity.java
+++ app/src/main/java/com/i4bchile/multipleitems/ResultActivity.java
@@ -1,15 +1,22 @@
 package com.i4bchile.multipleitems;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Objects;
 
 public class ResultActivity extends AppCompatActivity {
 
     Button openWeb;
     Button shareInfo;
+    String sitioWeb;
+    boolean imagenCargada;
 
     private void iniciarBotones() {
         openWeb=findViewById(R.id.bt_openweb);
@@ -17,13 +24,23 @@ public class ResultActivity extends AppCompatActivity {
         openWeb.setOnClickListener(new View.OnClickListener() {
                         @Override
             public void onClick(View v) {
-                // aqui abre www.desafiolatam.com
+                            String url=sitioWeb;
+                            Intent abrirWeb = new Intent(Intent.ACTION_VIEW);
+                            abrirWeb.setData(Uri.parse(url));
+                            startActivity(abrirWeb);
             }
         });
         shareInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //aqui abre un intent para compartir
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT,sitioWeb);
+                sendIntent.setType("text/plain");
+                Intent shareIntent = Intent.createChooser(sendIntent, null);
+                startActivity(shareIntent);
+
+
             }
         });
     }
@@ -31,9 +48,14 @@ public class ResultActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        Bundle datosMainActivity = getIntent().getExtras();
+        final String website = Objects.requireNonNull(datosMainActivity).getString( "WEBSITE" );
+        sitioWeb=website;
+        final boolean cargaImagen = Objects.requireNonNull(datosMainActivity).getBoolean( "IMAGEN CARGADA" );
+        imagenCargada=cargaImagen;
         iniciarBotones();
     }
 }
